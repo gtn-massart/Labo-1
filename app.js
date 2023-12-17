269c6d07223a4d58657a0c03f466870b32f82a73
Gaëtan Massart <129839243+gtn-massart@users.noreply.github.com>
2023-12-17 02:56:01 +0100

    responsive

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const burgerMenu = document.querySelector(".burger-menu");
+const navLinkList = document.querySelector(".nav-link-list");
+const inputContainer = document.querySelector(".input-container");
 
-  burgerMenu.addEventListener('click', e => {
-    e.target.classList.toggle('active');
-  })
   
+  burgerMenu.addEventListener('click', handleClick);
+
+  function handleClick() {
+
+    burgerMenu.classList.toggle('active');
+
+    if (burgerMenu.classList.contains("active")) {
+      navLinkList.style.display = "none";
+      inputContainer.style.display = "none";
+    } else {
+      navLinkList.style.display = "flex";
+      inputContainer.style.display = "flex";
+    }
+    
+  }
 
