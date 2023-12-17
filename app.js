f433cfae6401019d85ba155da52903fbd0708b53
Gaëtan Massart <129839243+gtn-massart@users.noreply.github.com>
2023-12-17 03:26:00 +0100

    refactoring

--- app.js
+++ app.js
@@ -3,19 +3,19 @@ const navLinkList = document.querySelector(".nav-link-list");
 const inputContainer = document.querySelector(".input-container");
 
   
-  burgerMenu.addEventListener('click', handleClick);
+burgerMenu.addEventListener('click', handleClick);
 
-  function handleClick() {
+function handleClick() {
 
-    burgerMenu.classList.toggle('active');
+  burgerMenu.classList.toggle('active');
 
-    if (burgerMenu.classList.contains("active")) {
-      navLinkList.style.display = "none";
-      inputContainer.style.display = "none";
-    } else {
-      navLinkList.style.display = "flex";
-      inputContainer.style.display = "flex";
-    }
-    
+  if (burgerMenu.classList.contains("active")) {
+    navLinkList.style.display = "none";
+    inputContainer.style.display = "none";
+  } else {
+    navLinkList.style.display = "flex";
+    inputContainer.style.display = "flex";
   }
+  
+}
 
